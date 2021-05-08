64bbfc5ea846b6f9f3fd93c6c43efc45f73bceee
Binod Sahu <sahubinod85@gmail.com>
2021-05-09 00:54:15 +0100

    add local state

--- src/components/PromoItems.jsx
+++ src/components/PromoItems.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/App.css";
 
 const PromoItems = (props) => {
   const {itemName, price, promotion, count, discount, total, calculatePromo} = props.item;
--- src/components/PromoTable.jsx
+++ src/components/PromoTable.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import PromoItems from './PromoItems'
 import config from '../appConfig'
 import "../css/App.css";
 
 const PromoTable = () => {
+  const [cartItems, setCartItems] = useState(config);
+  const [discountTotal, setDiscountTotal] = useState(0);
+  const [totalSub, setTotalSub] = useState(0);
+  const [amoutToPay, setAmountToPay] = useState(0);
 
-    const calculatePromo = () => {
-        console.log('called')
+  const calculatePromo = () => {
+      console.log('called')
     } 
+
     return (
-      <div className="container">
+        <div className="container">
         <table className="table caption-top">
             <caption className="text-center"><h1>Promo Table</h1></caption>
             <thead className="table-dark">
@@ -24,40 +29,40 @@ const PromoTable = () => {
                 </tr>
             </thead>
             <tbody>
-              { config.map((item, index) =>
-              <PromoItems 
+                { cartItems.map((item, index) =>
+                <PromoItems 
                 item= {item}
                 index={index}
                 key={index}
                 calculatePromo={calculatePromo}
-              />)
-              }  
-              
+                />)
+                }  
+                
             </tbody>
             <tfoot>
                 <tr>
                     <td className="fw-bolder" colSpan="4">Total</td>
-                    <td className="fw-bold">Total Price</td>
-                    <td className="fw-bold">Total Discount</td>
-                    <td className="fw-bold">Total Payable</td>
+                    <td className="fw-bold">£{totalSub}</td>
+                    <td className="fw-bold">£{discountTotal}</td>
+                    <td className="fw-bold">£{amoutToPay}</td>
                 </tr>
                 </tfoot>
             </table>  
 
             <div className="row text-end">
                 <div className="fs-4 fw-bold">Total Price :
-                    <span className="fw-light fs-5 ms-2">£</span>
+                    <span className="fw-light fs-5 ms-2">£{totalSub}</span>
                 </div>
                 <div className="fs-4 fw-bold">Total Discount :
-                    <span className="fw-light fs-5 ms-2">£</span>
+                    <span className="fw-light fs-5 ms-2">£{discountTotal}</span>
                 </div>
                 <div className="fs-4 fw-bold">Total Amount to pay :
-                    <span className="fw-light fs-5 ms-2">£</span>
+                    <span className="fw-light fs-5 ms-2">£{amoutToPay}</span>
                 </div>
 
             </div>
-      </div>
-    );
-  }
+        </div>
+        );
+    }
   
   export default PromoTable;
