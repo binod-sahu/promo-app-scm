31bc7ed232782c79570ec67bb3641185230af21f
Binod Sahu <sahubinod85@gmail.com>
2021-05-08 21:48:31 +0100

    create a dummy table by using bootstrap

--- src/components/PromoTable.jsx
+++ src/components/PromoTable.jsx
@@ -1,9 +1,39 @@
-
+import React from "react";
 
 const PromoTable = () => {
     return (
-      <div className="App">
-        <h1> My Promo App </h1>    
+      <div className="container">
+        <table class="table caption-top">
+            <caption className="text-center"><h1>Promo Table</h1></caption>
+            <thead class="table-dark">
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>  
       </div>
     );
   }
