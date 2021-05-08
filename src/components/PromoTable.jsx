e2ef84cfa6e1f2152b35fd6810d5f4df562a6d94
Binod Sahu <sahubinod85@gmail.com>
2021-05-08 22:34:52 +0100

    craete a dummy json and iterate

--- src/components/PromoTable.jsx
+++ src/components/PromoTable.jsx
@@ -2,20 +2,30 @@ import React from "react";
 import PromoItems from './PromoItems'
 
 const PromoTable = () => {
+    const test = [{firstName: 'mark', lastName: 'Otto', age: 40}, 
+    {firstName: 'Binod', lastName: 'sahu', age: 35},
+    {firstName: 'abc', lastName: 'def', age: 36}]
     return (
       <div className="container">
-        <table class="table caption-top">
+        <table className="table caption-top">
             <caption className="text-center"><h1>Promo Table</h1></caption>
-            <thead class="table-dark">
+            <thead className="table-dark">
                 <tr>
                 <th scope="col">#</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Age</th>
                 </tr>
             </thead>
             <tbody>
-                <PromoItems></PromoItems>
+              { test.map((item, index) =>
+              <PromoItems 
+                item= {item}
+                index={index}
+                key={index}
+              />)
+              }  
+              
             </tbody>
             </table>  
       </div>
