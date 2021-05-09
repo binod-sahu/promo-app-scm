b0de3bcf0ae6b6c7460f22905f07250caf92c7f4
Binod Sahu <sahubinod85@gmail.com>
2021-05-09 09:25:59 +0100

    add test case

--- src/__test__/PromoTable.test.js
+++ src/__test__/PromoTable.test.js
@@ -1,4 +1,24 @@
+import React from "react";
+import { render, fireEvent, act } from '@testing-library/react'
+import PromoTable from "../components/PromoTable";
+import renderer from 'react-test-renderer';
 
-test('test', () => {
-  expect(true).toBe(true)
+describe("snapshot", () => {
+  const props = {
+    itemName: "A",
+    price: 50,
+    count: 0,
+    discount: 0,
+    total: 0,
+    promoEngine: jest.fn()
+  };
+  test("should render snapshot", () => {
+    const renderedModule = renderer.create(<PromoTable />).toJSON();;
+    expect(renderedModule).toMatchSnapshot();
+  });
+
+  test("should render snapshot1 with props", () => {
+    const renderedModule = renderer.create(<PromoTable {...props} />).toJSON();;
+    expect(renderedModule).toMatchSnapshot();
+  });
 });
